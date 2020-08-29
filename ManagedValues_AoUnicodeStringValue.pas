b0831e6e5156cd4062b15113222efb7b21ea7290
František Milt <frantisek.milt@gmail.com>
2020-08-29 23:00:02 +0200

    Added copyright and license notices to all units

--- ManagedValues_AoUnicodeStringValue.pas
+++ ManagedValues_AoUnicodeStringValue.pas
@@ -1,3 +1,45 @@
+{-------------------------------------------------------------------------------
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+-------------------------------------------------------------------------------}
+{===============================================================================
+
+  Managed Values
+
+    Array managed value of UnicodeString.
+
+  Version 1.0 alpha (2020-08-29) - requires extensive testing
+
+  Last changed 2020-08-29
+
+  ©2020 František Milt
+
+  Contacts:
+    František Milt: frantisek.milt@gmail.com
+
+  Support:
+    If you find this code useful, please consider supporting its author(s) by
+    making a small donation using the following link(s):
+
+      https://www.paypal.me/FMilt
+
+  Changelog:
+    For detailed changelog and history please refer to this git repository:
+
+      github.com/TheLazyTomcat/ManagedValues
+
+  Dependencies:
+    AuxTypes        - github.com/TheLazyTomcat/Lib.AuxTypes
+    AuxClasses      - github.com/TheLazyTomcat/Lib.AuxClasses
+    BinaryStreaming - github.com/TheLazyTomcat/Lib.BinaryStreaming
+    StrRect         - github.com/TheLazyTomcat/Lib.StrRect
+    UInt64Utils     - github.com/TheLazyTomcat/Lib.UInt64Utils
+    ListSorters     - github.com/TheLazyTomcat/Lib.ListSorters
+
+===============================================================================}
 unit ManagedValues_AoUnicodeStringValue;
 
 {$INCLUDE './ManagedValues_defs.inc'}
